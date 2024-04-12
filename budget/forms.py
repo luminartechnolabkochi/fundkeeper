5702c228e627de4005dd264c3f319e03a7cb2242
sajay <sajaykannan10@gmail.com>
2024-04-12 10:02:37 +0530

    styles

--- budget/forms.py
+++ budget/forms.py
@@ -11,6 +11,19 @@ class ExpenseForm(forms.ModelForm):
 
         exclude=("id","created_date")
 
+        widgets={
+
+            "title":forms.TextInput(attrs={"class":"form-control"}),
+
+            "amount":forms.NumberInput(attrs={"class":"form-control"}),
+
+            "category":forms.Select(attrs={"class":"form-control form-select"}),
+
+            "owner":forms.TextInput(attrs={"class":"form-control"}),
+
+            "priority":forms.Select(attrs={"class":"form-control form-select"})
+        }
+
     
 
 
--- budget/views.py
+++ budget/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from django.views.generic import View
 
 from budget.forms import ExpenseForm
+
+from budget.models import Expense
+
 
 
 class ExpenseCreateView(View):
@@ -11,7 +14,26 @@ class ExpenseCreateView(View):
 
         form_instance=ExpenseForm()
 
-        return render(request,"expense_add.html",{"form":form_instance})
+        # orm qury for fecthing all expenses
+
+        qs=Expense.objects.all()
+
+
+        return render(request,"expense_add.html",{"form":form_instance,"data":qs})
+    
+    def post(self,request,*args,**kwargs):
+
+        form_instance=ExpenseForm(request.POST)
+
+        if form_instance.is_valid():
+
+            form_instance.save()
+
+            print("expense has been created")
+
+            return redirect("expense-add")
+        else:
+            return render(request,"expense_add.html",{"form":form_instance})
     
 
 
